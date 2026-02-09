28bc2939511654ebe6aab0d743cb0aa7be8185b6
PabloSCastillo <pablosebastiancastillo@gmail.com>
2026-02-09 18:58:04 -0300

    refactor: simplify App component and remove unused imports

--- porfolio/src/App.tsx
+++ porfolio/src/App.tsx
@@ -1,8 +1,4 @@
-import React, { useState, useEffect } from "react";
-import Preloader from "./components/Preloader/Preloader";
-import Navbar from "./components/Navbar/Navbar";
-import Section from "./components/Section/Section";
-import Footer from "./components/Footer/Footer";
+import React from "react";
 import Nav from "./components/Nav/Nav";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
@@ -10,11 +6,6 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 
 const App: React.FC = () => {
-  const [loading, setLoading] = useState(true);
-
-  const lorem =
-    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium ullam animi, necessitatibus earum, dolorum, officiis architecto sunt perferendis tempore nam ab vel aliquam! Possimus voluptates commodi debitis magnam officiis atque?";
-
   return (
     <>
       <Router>
